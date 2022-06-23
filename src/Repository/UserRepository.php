079c9046f0e8f560e769bbaa01fb96078577762f
Marijn van der Horst <marvanderhorst@deloitte.nl>
2022-06-23 16:59:11 +0200

    Paginate getAllUsers as well

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -8,11 +8,15 @@ use Auth0\SDK\Contract\API\ManagementInterface;
 use Auth0\SDK\Utility\HttpResponse;
 use Auth0\SDK\Utility\Request\FilteredRequest;
 use Auth0\SDK\Utility\Request\RequestOptions;
+use Closure;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Str;
+use JsonException;
+use Psr\Http\Message\ResponseInterface;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class UserRepository implements \Marketredesign\MrdAuth0Laravel\Contracts\UserRepository
@@ -44,6 +48,32 @@ class UserRepository implements \Marketredesign\MrdAuth0Laravel\Contracts\UserRe
     }
 
     /**
+     * Extract the content from an HTTP response as returned by the management API and parse as JSON.
+     * An HTTP exception is thrown when in case the request was not successful to begin with.
+     * An internal server error is generated when the JSON response could not be parsed.
+     *
+     * @param ResponseInterface $response
+     * @param int $expStatusCode The expected HTTP status code for the response to be considered successful.
+     * @param bool $associative When {@code true}, returned objects will be converted into associative arrays.
+     * @return mixed|void
+     */
+    private function decodeResponse(ResponseInterface $response, int $expStatusCode = 200, bool $associative = false)
+    {
+        // Throw exception if request was not successful.
+        if (!HttpResponse::wasSuccessful($response, $expStatusCode)) {
+            throw new HttpException($response->getStatusCode(), HttpResponse::getContent($response));
+        }
+
+        try {
+            // Return decoded response.
+            return json_decode(HttpResponse::getContent($response), $associative, 512, JSON_THROW_ON_ERROR);
+        } catch (JsonException $e) {
+            Log::error('An error occurred while decoding Auth0 management response.', $e->getTrace());
+            abort(500, 'Error while decoding Auth0 management response.');
+        }
+    }
+
+    /**
      * @inheritDoc
      */
     public function get($id): ?object
@@ -57,13 +87,11 @@ class UserRepository implements \Marketredesign\MrdAuth0Laravel\Contracts\UserRe
         return Cache::remember($cacheKey, $this->cacheTTL, function () use ($id) {
             $response = $this->mgmtApi->users()->get($id);
 
-            if (HttpResponse::wasSuccessful($response)) {
-                return json_decode($response->getBody());
-            } elseif (HttpResponse::getStatusCode($response) == 404) {
+            if (HttpResponse::getStatusCode($response) == 404) {
                 return null;
-            } else {
-                throw new HttpException(HttpResponse::getStatusCode($response), HttpResponse::getContent($response));
             }
+
+            return $this->decodeResponse($response, 200, false);
         });
     }
 
@@ -77,8 +105,9 @@ class UserRepository implements \Marketredesign\MrdAuth0Laravel\Contracts\UserRe
         }
 
         $response = $this->mgmtApi->users()->delete($id);
-        if ($response->getStatusCode() != 204) {
-            throw new HttpException($response->getStatusCode());
+
+        if (!HttpResponse::wasSuccessful($response, 204)) {
+            throw new HttpException(HttpResponse::getStatusCode($response), HttpResponse::getContent($response));
         }
     }
 
@@ -96,12 +125,8 @@ class UserRepository implements \Marketredesign\MrdAuth0Laravel\Contracts\UserRe
             'password' => Hash::make(Str::random())
         ]);
 
-        if ($response->getStatusCode() != 201) {
-            throw new HttpException($response->getStatusCode());
-        }
-
         // body of response should be the newly created user object
-        return json_decode($response->getBody());
+        return $this->decodeResponse($response, 201);
     }
 
     /**
@@ -141,9 +166,9 @@ class UserRepository implements \Marketredesign\MrdAuth0Laravel\Contracts\UserRe
                 // Create Lucene query on user IDs.
                 $query = $queryField . ':("' . implode('" OR "', $qValsChunk->all()) . '")';
                 // Send request to Auth0 Management API.
-                $response = $this->mgmtApi->users()->getAll(['q' => $query], $options);
+                $response = $this->decodeResponse($this->mgmtApi->users()->getAll(['q' => $query], $options));
                 // Find users in the response and add to collection.
-                $users = $users->concat(json_decode($response->getBody()));
+                $users = $users->concat($response);
             }
 
             return collect($users)->keyBy($queryField);
@@ -159,25 +184,62 @@ class UserRepository implements \Marketredesign\MrdAuth0Laravel\Contracts\UserRe
     }
 
     /**
-     * @inheritdoc
-     */
-    public function getAllUsers(): Collection
-    {
-        $response = $this->mgmtApi->users()->getAll();
-        if ($response->getStatusCode() != 200) {
-            throw new HttpException($response->getStatusCode());
-        }
-
-        $users = json_decode($response->getBody());
-
-        return collect($users)->keyBy('user_id');
-    }
-
-    /**
      * @inheritDoc
      */
     public function getByEmails(Collection $emails, array $fields = null): Collection
     {
         return $this->getAll('email', $emails, $fields);
+    }
+
+    /**
+     * Calls the management API using {@code $callApi} as often as required and combines the results of the different
+     * pages into one collection.
+     *
+     * @param string $responseKey The key in the response from the management API containing the desired data.
+     * @param Closure $callApi Function that makes the actual calls to the management API. It must accept a param array
+     *  that is passed to the calls to the management API (containing the requested page etc).
+     * @return Collection
+     */
+    private function getFromMgmtPaginated(string $responseKey, Closure $callApi)
+    {
+        // Keep track of current page.
+        $page = 0;
+        // Function to create management API parameters including the requested page.
+        $createParams = fn($page) => ['include_totals' => true, 'page' => $page];
+        // Perform initial call to management API and decode the response.
+        $initialResponse = $this->decodeResponse($callApi($createParams($page)));
+        // Collect the desired results from the response.
+        $result = collect($initialResponse->$responseKey);
+        // Find page metadata in the response.
+        $total = $initialResponse->total;
+        $start = $initialResponse->start;
+        $length = $initialResponse->length;
+
+        // We keep calling the management API until there is no data left.
+        while ($start + $length < $total) {
+            $page++;
+            // Get new response for the next page.
+            $response = $this->decodeResponse($callApi($createParams($page)));
+            // Add the results of the new response to the collection.
+            $result = $result->concat($response->$responseKey);
+            // Update start and length values such that we know when we are finished.
+            $start = $response->start;
+            $length = $initialResponse->length;
+        }
+
+        // Finally, return the collection with the combined results.
+        return $result;
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function getAllUsers(): Collection
+    {
+        return Cache::remember('auth0-all-users', $this->cacheTTL, function () {
+            $users = $this->getFromMgmtPaginated('users', fn($params) => $this->mgmtApi->users()->getAll($params));
+
+            return $users->keyBy('user_id');
+        });
     }
 }
